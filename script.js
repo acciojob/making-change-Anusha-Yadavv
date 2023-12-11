a8028b489931e4fee05cbddf581d2ff7ab1dce9c
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-11 14:14:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const makeChange = (c) => {
   // your name here
+	const quarterValue = 25;
+  const dimeValue = 10;
+  const nickelValue = 5;
+
+  // Calculate the number of quarters
+  const q = Math.floor(cents / quarterValue);
+  cents %= quarterValue;
+
+  // Calculate the number of dimes
+  const d = Math.floor(cents / dimeValue);
+  cents %= dimeValue;
+
+  // Calculate the number of nickels
+  const n = Math.floor(cents / nickelValue);
+  cents %= nickelValue;
+
+  // The remaining cents are the number of pennies
+  const p = cents;
+
+  return { q, d, n, p };
 };
 
 // Do not the change the code below
